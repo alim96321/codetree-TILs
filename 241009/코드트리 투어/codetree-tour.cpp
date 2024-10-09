f7e10aea02d59dada43defc0eb7ca7c47f6f5342
SangukLee <129043068+alim96321@users.noreply.github.com>
2024-10-09 16:25:10 +0900

    Title: 코드트리 투어, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241009/코드트리 투어/codetree-tour.cpp
+++ 241009/코드트리 투어/codetree-tour.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
 #include <climits>
 #include <map>
 
@@ -9,22 +8,18 @@ using namespace std;
 #define N_MAX 2005
 #define M_MAX 10005
 
-struct Products{
-    int plus, id;
-
-    Products(int a, int b) : plus(a), id(b) {}
+// first : 이윤(매출-거리), second : id
+struct cmp{
+    bool operator()(pair<int, int>& a, pair<int, int>& b){
+        if(a.first != b.first) return a.first < b.first;
+        return a.second > b.second;
+    }
 };
 
 vector<vector<pair<int, int>>> graph(N_MAX, vector<pair<int, int>>());  // (도착지, 비용)
-vector<Products> thing; // (이윤-거리, id)
 map<int, pair<int, int>> product;  // (이윤, 도착지)
-
-int start = 0; // 출발지
-
-bool compare(Products a, Products b){
-    if(a.plus != b.plus) return a.plus > b.plus;
-    return a.id < b.id;
-}
+map<int, bool> arr_id;  // (id, true or false)
+priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> ppq;
 
 // node간의 최소 거리 최신화
 void Dijkstra(int distance[N_MAX],int st){
@@ -80,44 +75,36 @@ int main() {
                     if(v != u)
                         graph[u].emplace_back(v, w);
                 }
-                Dijkstra(distance, start);
+                Dijkstra(distance, 0);
                 break;
             }
             case 200:{
                 int id, rev, dest;
                 cin >> id >> rev >> dest;
                 product[id] = make_pair(rev, dest);
-                thing.emplace_back(Products(rev-distance[dest], id));
+                ppq.push(make_pair(rev-distance[dest], id));
+                arr_id[id] = true;
                 break;
             }
             case 300:{
                 int id;
                 cin >> id;
                 product.erase(id);
-                
-                int num = thing.size();
-                vector<Products> tmp;
-                for(int j = 0; j < num; j++){
-                    if(thing[j].id == id){
-                        continue;
-                    }
-                    tmp.emplace_back(Products(thing[j].plus, thing[j].id));
-                }
-                thing = tmp;
+                arr_id[id] = false;
                 
                 break;
             }
             case 400:{
-                if(thing.empty()){
+                if(ppq.empty()){
                     cout << -1 << '\n';
                 }
                 else{
-                    sort(thing.begin(), thing.end(), compare);
-                    Products np = thing.front();
-                    if(distance[product[np.id].second] != INT_MAX && np.plus >= 0){
-                        cout << np.id << '\n';
-                        thing.erase(thing.begin());
-                        product.erase(np.id);
+                    pair<int, int> np = ppq.top();
+                    if((distance[product[np.second].second] != INT_MAX) && (np.first >= 0) && (arr_id[np.second])){
+                        cout << np.second << '\n';
+                        arr_id[np.second] = false;
+                        ppq.pop();
+                        product.erase(np.second);
                     }
                     else{
                         cout << -1 << '\n';
@@ -129,13 +116,15 @@ int main() {
                 int s;
                 cin >> s;
                 Dijkstra(distance, s);
-                start = s;
 
-                int num = thing.size();
+                int num = ppq.size();
+                priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> tmp;
                 for(int j = 0; j < num; j++){
-                    int tmp_id = thing[j].id;
-                    thing[j].plus = product[tmp_id].first - distance[product[tmp_id].second];
+                    int tmp_id = ppq.top().second;
+                    ppq.pop();
+                    tmp.push(make_pair(product[tmp_id].first - distance[product[tmp_id].second], tmp_id));
                 }
+                ppq = tmp;
                 break;
             }
         }
